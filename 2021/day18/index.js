4162f60cda351ad4cc9f78523746289a37bc94ac
Mike Nelson <ralphpig@gmail.com>
2022-07-28 00:23:43 -0400

    WIP day18

--- 2021/day18/index.js
+++ 2021/day18/index.js
@@ -8,6 +8,11 @@ const STATE = {
   DONE: 5,
 };
 
+const TYPE = {
+  PAIR: 1,
+  NODE: 2,
+};
+
 class state_machine {
   constructor(data = {}) {
     this.states = {
@@ -37,6 +42,11 @@ class state_machine {
             ...data,
           };
           this.state = STATE.DONE;
+          this.data.type =
+            typeof this.data.left === 'number' &&
+            typeof this.data.right === 'number'
+              ? TYPE.PAIR
+              : TYPE.NODE;
         },
       },
     };
@@ -61,7 +71,7 @@ function get_input(input) {
     .split('\n')
     .filter((line) => line)
     .map((line) => {
-      return parse(line);
+      return parse(line.trim());
     });
 }
 
@@ -72,6 +82,7 @@ function parse(iter, initial = null, depth = 0) {
   const fsm = new state_machine({
     left: null,
     right: null,
+    type: null
   });
 
   do {
@@ -113,8 +124,114 @@ function parse(iter, initial = null, depth = 0) {
   return fsm.data;
 }
 
-{
-  const input = get_input('[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]');
+function* traverse(node, ltr = true) {
+  if (node.left === 'number' && typeof node.right === ) yield node;
+  if (ltr) {
+    yield* traverse(node.left, ltr);
+    yield* traverse(node.right, ltr);
+  } else {
+    yield* traverse(node.right, ltr);
+    yield* traverse(node.left, ltr);
+  }
+}
 
-  console.log(JSON.stringify(input, null, 2));
+function add(left, right) {
+  return reduce({
+    left,
+    right,
+  }).node;
+}
+
+function reduce(node, depth = 0) {
+  console.log(depth, node);
+  if (typeof node === 'number') {
+    if (node >= 10) {
+      return {
+        node: {
+          left: Math.floor(node / 2),
+          right: Math.ceil(node / 2),
+          type: TYPE.PAIR
+        },
+      };
+    }
+
+    return {
+      node,
+    };
+  }
+
+  if (
+    depth >= 4 &&
+    typeof node.left === 'number' &&
+    typeof node.right === 'number'
+  ) {
+    return {
+      node: 0,
+      action: node,
+    };
+  }
+
+  const out = {
+    node: {},
+    action: undefined,
+    type: TYPE.NODE
+  };
+
+  console.log(depth, 'left', node);
+  const { node: left_node, action: left_action = null } = reduce(
+    node.left,
+    depth + 1
+  );
+  console.log(depth, 'left', left_node, left_action);
+
+  out.node.left = left_node;
+
+  console.log(depth, 'right');
+  const { node: right_node, action: right_action = null } = reduce(
+    node.right,
+    depth + 1
+  );
+  console.log(depth, 'right', right_node, right_action);
+
+  out.node.right = right_node;
+
+  if (left_action) {
+
+  }
+  if (left_action || right_action) {
+    // out.node.left += right_action?.left || 0;
+    // out.node.right += left_action?.right || 0;
+    out.action = {
+      left: left_action?.left,
+      right: right_action?.right,
+    };
+  }
+
+  if (!out.left) console.log(depth, 'NO LEFT');
+  console.log(depth, 'out', out);
+  return out;
+}
+
+// {
+//   const input = get_input(`
+//     [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]
+//   `);
+
+//   const out = input.reduce((o, node) => add(o, node));
+
+//   console.log(JSON.stringify(out, null, 2));
+// }
+
+{
+  const input = get_input(`
+    [1,1]
+    [2,2]
+    [3,3]
+    [4,4]
+    [5,5]
+  `);
+
+  const out = input.reduce((o, node) => add(o, node));
+
+  console.log(JSON.stringify(out, null, 2));
 }
