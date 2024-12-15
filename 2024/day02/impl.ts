d55d3f943cfefba43c2b98a4acaf90daf3645feb
Mike <mail@ralphpig.dev>
2024-12-15 14:06:59 -0500

    2024(day02)

--- 2024/day02/impl.ts
+++ 2024/day02/impl.ts
@@ -7,6 +7,7 @@ export function parse_input(lines: string[]): number[][] {
     });
 }
 
+// Return set of altered reports to re-test
 type ProblemDampener = (report: number[], index: number | string) => number[][];
 
 function is_safe(report: number[], dampener?: ProblemDampener): boolean {
@@ -14,16 +15,11 @@ function is_safe(report: number[], dampener?: ProblemDampener): boolean {
   let last;
   let last_dir;
 
-  console.log({
-    report,
-    dampener: Boolean(dampener),
-  });
-
   const run_dampener = (index: string | number) => {
     if (!dampener) return false;
     return dampener(report, index)
       .map((new_report) => is_safe(new_report))
-      .some((safe) => safe);
+      .some(Boolean);
   };
 
   for (const index in report) {
@@ -36,12 +32,6 @@ function is_safe(report: number[], dampener?: ProblemDampener): boolean {
     const diff = level - last;
     const abs_diff = Math.abs(diff);
     const dir = diff / abs_diff; // +1 or -1
-
-    console.log({
-      window: [last, level],
-      diff,
-      dir_window: [last_dir, dir],
-    });
 
     // Any two adjacent levels differ by at least one and at most three.
     if (!(abs_diff >= 1 && abs_diff <= 3)) {
@@ -59,12 +49,6 @@ function is_safe(report: number[], dampener?: ProblemDampener): boolean {
     last = level;
   }
 
-  console.log({
-    report,
-    safe,
-    dampener: Boolean(dampener),
-  });
-
   return safe;
 }
 
@@ -77,9 +61,6 @@ export function solve_one(input: number[][]): number {
 
   for (const report of input) {
     if (is_safe(report)) {
-      console.log({
-        safe: report,
-      });
       total_safe++;
     }
   }
@@ -91,18 +72,30 @@ export function solve_two(input: number[][]): number {
   let total_safe = 0;
 
   const dampener: ProblemDampener = (report, index) => {
-    const left = [...report];
-    left.splice(Number(index) - 1, 1);
-    const curr = [...report];
-    curr.splice(Number(index), 1);
+    index = Number(index);
+    const out = [];
 
-    return [left, curr];
+    if (index - 2 >= 0) {
+      const n = [...report];
+      n.splice(index - 2, 1);
+      out.push(n);
+    }
+
+    if (index - 1 >= 0) {
+      const n = [...report];
+      n.splice(index - 1, 1);
+      out.push(n);
+    }
+
+    const n = [...report];
+    n.splice(index, 1);
+    out.push(n);
+
+    return out;
   };
+
   for (const report of input) {
     if (is_safe(report, dampener)) {
-      console.log({
-        safe: report,
-      });
       total_safe++;
     }
   }
